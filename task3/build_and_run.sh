caab28aac88e0a7d4a67303069f21dd26f085446
Rand Deeb <70079844+rand-itmo-wad@users.noreply.github.com>
2022-09-26 21:46:07 +0300

    fix build_and_run.sh

--- task3/build_and_run.sh
+++ task3/build_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t jenkinsNTP
-docker run -p=123:123/udp jenkinsNTP
+docker build . -t jenkinsNTP -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)  -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
+docker run -p=123:123/udp jenkinsNTP -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)  -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
