c37f8010d270d7e7759d9d7c657716076cb98b73
Rand Deeb <70079844+rand-itmo-wad@users.noreply.github.com>
2022-09-26 21:58:25 +0300

    Update build_and_run.sh

--- task3/build_and_run.sh
+++ task3/build_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t jenkinsNTP -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)
-docker run -p=123:123/udp jenkinsNTP -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)
+docker build . -t jenkinsNTP -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker
+docker run -p=123:123/udp jenkinsNTP -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker
